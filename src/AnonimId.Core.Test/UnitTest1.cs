0b9acbc73a6efc9116c5e2f5e225111824eee530
Duke <duke@live.it>
2023-10-09 14:55:22 +0200

    working on tests

--- src/AnonimId.Core.Test/UnitTest1.cs
+++ src/AnonimId.Core.Test/UnitTest1.cs
@@ -1,11 +1,54 @@
+
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+
 namespace AnonimId.Core.Test
 {
-    public class UnitTest1
+    public class DatabaseTest : IAsyncLifetime
     {
+        string username = "user";
+        string password = "pa$$";
+        IContainer surrealDbContainer= null!;
+
+
+
+
+        public async Task InitializeAsync()
+        {
+            
+
+            surrealDbContainer = new ContainerBuilder().WithImage("surrealdb/surrealdb:latest")
+            .WithPortBinding(8000, true)            
+            .WithCommand("start", "--auth", "--user", username, "--pass", password)            
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8000)))
+            .Build();
+
+            await surrealDbContainer.StartAsync();
+
+            var hostPort =surrealDbContainer.GetMappedPublicPort(8000);
+
+        }
+
+        public async Task DisposeAsync()
+        {
+            await surrealDbContainer.StopAsync();
+
+        }
+
         [Fact]
         public void Test1()
         {
-
+            int i = 0;
+        }
+        [Fact]
+        public void Test2()
+        {
+            int i = 0;
+        }
+        [Fact]
+        public void Test3()
+        {
+            int i = 0;
         }
     }
 }
